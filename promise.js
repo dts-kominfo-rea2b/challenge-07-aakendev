0544c318023e5f9ed547ffd2d32418ba33a25c7b
AAKEN Developer <aakendev@gmail.com>
2022-06-18 23:30:55 +0700

    daritadi salah terus ternyata lupa parameter nya haha

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    let scoreEmosi = 0;
+    promiseTheaterIXX()
+      .then((hasilSetelahMenonton) => {
+        hasilSetelahMenonton.forEach((item) => {
+          if (item.hasil === emosi) {
+            scoreEmosi += 1;
+          }
+        });
+      })
+      .then(
+        promiseTheaterVGC().then((hasilSetelahMenonton) => {
+          hasilSetelahMenonton.forEach((item) => {
+            if (item.hasil === emosi) {
+              scoreEmosi += 1;
+            }
+          });
+          resolve(scoreEmosi);
+        })
+      );
+  });
+};
 
 module.exports = {
   promiseOutput,
